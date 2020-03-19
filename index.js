90dfb2bcd5c5bb8cdbf148a24357d149a5b7bb2f
vienpham2019 <vienpham2019@gmail.com>
2020-03-18 21:49:16 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+const mapToNegativize = (arr) => arr.map(element => element * -1)
+
+const mapToNoChange = (arr) => arr.map(element => element)
+
+const mapToDouble = (arr) => arr.map(element => element * 2)
+
+const mapToSquare = (arr) => arr.map(element => element ** 2)
+
+const reduceToTotal = (arr,star_point = 0) => arr.reduce((sum,e) => sum + e , star_point)
+
+const reduceToAllTrue = (arr) => arr.every(element => !!element === true)
+
+const reduceToAnyTrue = (arr) => arr.some(element => !!element === true)
